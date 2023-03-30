ed946788e6fe5da03414e2875957838fde052b5d
Louis-Philippe Perron <32278+lp@users.noreply.github.com>
2023-03-30 15:38:14 -0400

    chatbot with chained prompts & cleaned instructions

--- packages/campfhir-cli/src/commands/assistant.ts
+++ packages/campfhir-cli/src/commands/assistant.ts
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
 import { readFile } from "fs/promises";
 
+import { LLMChain } from "langchain";
+import { ChatOpenAI } from "langchain/chat_models";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
 import prompts from "prompts";
 import { CommandModule } from "yargs";
 
-import { OpenAI } from "langchain/llms";
 import { FewShotPromptTemplate, PromptTemplate } from "langchain/prompts";
 
 const trainingDataFilePath = "/workspace/data/prompts.jsonl";
@@ -28,55 +34,54 @@ All other classes are unknown.
 Classes names are case insensitive. There are no subclasses or roles derived from the known classes.
 If you are asked for an unknown class you should answer: "Sorry, I don't know about CLASS", interpolating "CLASS" with the unknown class name.
 
-Each class has a known set of valid querystring for answer:
-
-The only known querystrings valid for RiskAssessment are: subject, risk, _summary.
-The only known querystrings valid for CarePlan are: status, intent, title, _summary.
-
-All other querystring are unknown.
-The answer can only contain known querystring.
-
-If the querystring is invalid or unknown, you should answer: "Sorry, I don't know the parameter QUERYSTRING", interpolating "QUERYSTRING" with the unknown or invalid querystring.
-
-** EXAMPLES**
-`;
+** EXAMPLES**`;
 
     const examplePrompt = new PromptTemplate({
       inputVariables: ["prompt", "completion"],
       template: exampleFormatterTemplate,
     });
 
-    console.log("examplePrompt: ", examplePrompt);
+    // console.log("examplePrompt: ", examplePrompt);
 
     const fewShotPrompt = new FewShotPromptTemplate({
       examples,
       examplePrompt,
       prefix: instructions,
-      suffix:
-        "What API path would answer the question below?\nQ: {prompt} ||| api:",
-      inputVariables: ["prompt"],
       exampleSeparator: "\n\n",
       templateFormat: "f-string",
+      inputVariables: [],
     });
 
-    console.log("fewShotPrompt: ", fewShotPrompt);
+    //console.log("fewShotPrompt: ", fewShotPrompt);
+
+    const instructionPrompt = SystemMessagePromptTemplate.fromTemplate(
+      await fewShotPrompt.format({})
+    );
+    const questionPrompt = HumanMessagePromptTemplate.fromTemplate(
+      "What API path would answer the question below?\nQ: {question} ||| api:"
+    );
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+      instructionPrompt,
+      questionPrompt,
+    ]);
+    //console.log("chatPrompt: ", chatPrompt);
 
     dotenv.config(); // get the OpenAPI key from .env file
 
-    const text = "Q";
+    const chat = new ChatOpenAI({ temperature: 0 });
+    // console.log("chat: ", chat);
 
-    const query = await prompts({
-      type: "text",
-      name: "prompt",
-      message: text,
-    });
+    const chain = new LLMChain({ llm: chat, prompt: chatPrompt });
 
-    const prompt = await fewShotPrompt.format({ prompt: query.prompt });
+    while (true) {
+      const query = await prompts({
+        type: "text",
+        name: "question",
+        message: "Q", // pass Chatbot message here?
+      });
 
-    console.log("prompt: ", prompt);
-
-    const llm = new OpenAI({ temperature: 0 });
-    const apiPath = await llm.call(prompt);
-    console.log("apiPath: ", apiPath);
+      const result = await chain.call({ question: query.question });
+      console.log("result: ", result);
+    }
   },
 };
