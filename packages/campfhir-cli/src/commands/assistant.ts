cc6f897fcc174c0936529c83a2d6e5afe8e3a484
Louis-Philippe Perron <32278+lp@users.noreply.github.com>
2023-03-30 14:36:06 -0400

    provide instructions to model, plus script for assistant

--- packages/campfhir-cli/src/commands/assistant.ts
+++ packages/campfhir-cli/src/commands/assistant.ts
@@ -22,20 +22,44 @@ export default <CommandModule>{
   handler: async (_options) => {
     const examples = await loadTrainingData();
     const exampleFormatterTemplate = "Q: {prompt} ||| api: {completion}";
+    const instructions = `** INSTRUCTIONS **
+The only known api classes valid for answer are: Patient, RiskAssessment, Practitioner, Appointment, CarePlan.
+All other classes are unknown.
+Classes names are case insensitive. There are no subclasses or roles derived from the known classes.
+If you are asked for an unknown class you should answer: "Sorry, I don't know about CLASS", interpolating "CLASS" with the unknown class name.
+
+Each class has a known set of valid querystring for answer:
+
+The only known querystrings valid for RiskAssessment are: subject, risk, _summary.
+The only known querystrings valid for CarePlan are: status, intent, title, _summary.
+
+All other querystring are unknown.
+The answer can only contain known querystring.
+
+If the querystring is invalid or unknown, you should answer: "Sorry, I don't know the parameter QUERYSTRING", interpolating "QUERYSTRING" with the unknown or invalid querystring.
+
+** EXAMPLES**
+`;
+
     const examplePrompt = new PromptTemplate({
       inputVariables: ["prompt", "completion"],
       template: exampleFormatterTemplate,
     });
 
+    console.log("examplePrompt: ", examplePrompt);
+
     const fewShotPrompt = new FewShotPromptTemplate({
       examples,
       examplePrompt,
-      prefix: "What API path would answer the question below?",
-      suffix: "Q: {prompt} ||| api:",
+      prefix: instructions,
+      suffix:
+        "What API path would answer the question below?\nQ: {prompt} ||| api:",
       inputVariables: ["prompt"],
       exampleSeparator: "\n\n",
       templateFormat: "f-string",
     });
+
+    console.log("fewShotPrompt: ", fewShotPrompt);
 
     dotenv.config(); // get the OpenAPI key from .env file
 
@@ -48,6 +72,8 @@ export default <CommandModule>{
     });
 
     const prompt = await fewShotPrompt.format({ prompt: query.prompt });
+
+    console.log("prompt: ", prompt);
 
     const llm = new OpenAI({ temperature: 0 });
     const apiPath = await llm.call(prompt);
