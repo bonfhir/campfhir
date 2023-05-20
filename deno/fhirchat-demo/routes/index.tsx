7050b6f51a9e40bcce9f81e534d2e69b7d09eeb4
Louis-Philippe Perron <32278+lp@users.noreply.github.com>
2023-05-20 16:07:46 +0000

    clean-out fresh vanilla & some .env samples

--- deno/fhirchat-demo/routes/index.tsx
+++ deno/fhirchat-demo/routes/index.tsx
@@ -6,19 +6,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>CampFHIR</title>
       </Head>
       <div>
-        <img
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p>
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
+        <p>Uncover insights from you EHR</p>
         <AIQnA />
       </div>
     </>
